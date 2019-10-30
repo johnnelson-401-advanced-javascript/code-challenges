877b31c8dbd6477513c6145433c63a90086441dc
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-30 13:43:06 -0700

    passing Merge Sort, fixed the run away arrays.

--- __tests__/merge-sort.test.js
+++ __tests__/merge-sort.test.js
@@ -3,6 +3,6 @@ const mergeSort = require('../merge-sort');
 describe('Tests mergeSort', () => {
   it.only('Sorts an array of integers', () => {
     let arr = [4, 3, 5, 1, 2];
-    expect(mergeSort(arr)).toBe([1, 2, 3, 4, 5]);  
+    expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 5]);  
   });
 });
--- merge-sort.js
+++ merge-sort.js
@@ -1,38 +1,35 @@
-const mergeSort = (arr) => {
+function mergeSort(arr) {
   let n = arr.length;
   
-  if(n < 2) {
+  if(n === 1) {
     return arr;
-  } else {
-    const mid = parseInt(n / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid, n);
-    mergeSort(left);
-    mergeSort(right);
-    merge(left, right, arr);
   }
-};
-const merge = (right, left, arr) => {
+  const mid = Math.floor(n / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  );
+}
+
+
+function merge(right, left) {
   let i = 0;
   let j = 0;
-  let k = 0;
+  let result = [];
   while(i < left.length && j < right.length) {
     if(left[i] <= right[j]) {
-      arr[k] = left[i];
+      result.push(left[i]);
+      i++;
     } else {
-      arr[k] = arr[j];
-      j++;
-    }
-    k++;
-  }
-  if(i === left.length) {
-    while(j < right.length) {
-      arr[k] = right[j];
-      k++;
+      result.push(right[j]);
       j++;
     }
   }
-  return arr;
-};
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+
 
 module.exports = mergeSort;
